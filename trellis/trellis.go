158c850be9473b919d0bd3440cedc098288f5793
Steve Zatz <slzatz@gmail.com>
2022-07-07 15:01:20 -0400

    trellis.go: first commit for adafruit trellis driver

---
+++ trellis/trellis.go
@@ -0,0 +1,188 @@
+package trellis
+
+import "tinygo.org/x/drivers"
+
+// HT16K33 Command Contstants
+const (
+	HT16K33_OSCILATOR_ON    = 0x21
+	HT16K33_BLINK_CMD       = 0x80
+	HT16K33_BLINK_DISPLAYON = 0x01
+	HT16K33_CMD_BRIGHTNESS  = 0xE0
+	HT16K33_KEY_READ_CMD    = 0x40
+
+	Address = 0x70
+)
+const (
+	HT16K33_BLINK_OFF    = 0
+	HT16K33_BLINK_2HZ    = 1
+	HT16K33_BLINK_1HZ    = 2
+	HT16K33_BLINK_HALFHZ = 3
+)
+
+var displaybuffer = [8]uint16{}
+
+// LED Lookup Table
+
+var (
+	displaybuffer [8]uint16
+	keys          [8]uint8
+	lastkeys      [8]uint8
+
+	ledLUT = [16]int{
+		0x3A, 0x37, 0x35, 0x34,
+		0x28, 0x29, 0x23, 0x24,
+		0x16, 0x1B, 0x11, 0x10,
+		0x0E, 0x0D, 0x0C, 0x02}
+
+	buttonLUT = [16]int{
+		0x07, 0x04, 0x02, 0x22,
+		0x05, 0x06, 0x00, 0x01,
+		0x03, 0x10, 0x30, 0x21,
+		0x13, 0x12, 0x11, 0x31}
+)
+
+type Device struct {
+	bus     drivers.I2C
+	Address uint16
+}
+
+func New(bus drivers.I2C) Device {
+	return Device{bus, Address}
+}
+
+func (d Device) Configure() bool {
+	//d.bus.WriteRegister(uint8(d.Address), HT16K33_OSCILATOR_ON, []uint8{0})
+	err := d.bus.Tx(d.Address, []byte{0x21}, nil)
+	if err != nil {
+		println("Could not turn oscillator on")
+		return
+	}
+	blinkRate(HT16K33_BLINK_OFF)
+	setBrightness(15) // max brightness
+	d.bus.Tx(d.Address, []byte{0xA1}, nil)
+}
+
+//bool Adafruit_Trellis::isKeyPressed(uint8_t k) {
+func isKeyPressed(k uint8) bool {
+	if k > 15 {
+		return false
+	}
+	key = buttonLUT[k]
+	//#define _BV(bit) (1 << (bit))
+	return (keys[key>>4] & (1 << (key & 0x0F)))
+}
+
+//bool Adafruit_Trellis::wasKeyPressed(uint8_t k) {
+func wasKeyPressed(k uint8) bool {
+	if k > 15 {
+		return false
+	}
+	key = buttonLUT[k]
+	return (lastkeys[key>>4] & (1 << (key & 0x0F)))
+}
+
+//boolean Adafruit_Trellis::justPressed(uint8_t k) {
+func justPressed(k uint8) bool {
+	return (isKeyPressed(k) & !wasKeyPressed(k))
+}
+
+//boolean Adafruit_Trellis::justReleased(uint8_t k) {
+func justReleased(k uint8) bool {
+	return (!isKeyPressed(k) & wasKeyPressed(k))
+}
+
+//boolean Adafruit_Trellis::isLED(uint8_t x) {
+func isLED(x uint8) bool {
+	if x > 15 {
+		return false
+	}
+	led = ledLUT[x]
+	//#define _BV(bit) (1 << (bit))
+	return (displaybuffer[led>>4]&(1<<(led&0x0F)) > 0)
+}
+
+//void Adafruit_Trellis::setLED(x uint8) {
+func setLED(x uint8) bool {
+	if x > 15 {
+		return
+	}
+	led = ledLUT[x]
+	displaybuffer[led>>4] |= (1 << (led & 0x0F))
+}
+
+//void Adafruit_Trellis::clrLED(x uint8) {
+func clrLED(x uint8) bool {
+	if x > 15 {
+		return
+	}
+	led = ledLUT[x]
+	displaybuffer[led>>4] &= (1 << (led & 0x0F))
+}
+
+//boolean Adafruit_Trellis::readSwitches(void) {
+func (d Device) readSwitches() bool {
+	//memcpy(lastkeys, keys, sizeof(keys));
+	lastkeys := keys
+
+	//Wire.write(0x40);
+	//Wire.requestFrom((byte)i2c_addr, (byte)6);
+	var buf [6]byte
+	d.bus.Tx(d.Address, []byte{uint8(0x40)}, buf[:])
+
+	/*
+		  Wire.requestFrom((byte)i2c_addr, (byte)6);
+			for i:=0; i<6; i++
+		    keys[i] = Wire.read();
+	*/
+
+	copy(keys[:], buf)
+
+	for i := 0; i < 6; i++ {
+		if lastkeys[i] != keys[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func (d Device) setBrightness(b uint8) {
+	if b > 15 {
+		b = 15
+	}
+	d.bus.Tx(d.Address, []byte{HT16K33_CMD_BRIGHTNESS | b}, nil)
+}
+
+func (d Device) blinkRate(b uint8) {
+	if b > 3 {
+		b = 0
+	}
+	x := HT16K33_BLINK_CMD | HT16K33_BLINK_DISPLAYON | (b << 1)
+	d.bus.Tx(d.Address, []byte{x}, nil)
+}
+
+//void Adafruit_Trellis::writeDisplay(void) {
+func (d Device) writeDisplay() {
+	//Wire.write((uint8_t)0x00); // start at address $00
+	err := d.bus.Tx(d.Address, []byte{0x00}, nil)
+	if err != nil {
+		println("Could not write")
+		return
+	}
+
+	for i := 0; i < 8; i++ {
+		//  Wire.write(displaybuffer[i] & 0xFF);
+		// this is the first byte of the uint16
+		d.bus.Tx(d.Address, []byte{uint8(displaybuffer[i] & 0xFF)}, nil)
+		//  Wire.write(displaybuffer[i] >> 8);
+		// this is the second byte of the uint16
+		d.bus.Tx(d.Address, []byte{uint8(displaybuffer[i] >> 8)}, nil)
+	}
+}
+
+//void Adafruit_Trellis::clear(void) {
+func clear() {
+	//memset(displaybuffer, 0, sizeof(displaybuffer));
+	for i := range displaybuffer {
+		displaybuffer[i] = 0
+	}
+}
