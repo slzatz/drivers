04555a430468f70307a0245b30763a909b5a1675
Steve Zatz <slzatz@gmail.com>
2022-07-18 16:18:08 -0400

    trellis.go: cleaned up code

--- trellis/trellis.go
+++ trellis/trellis.go
@@ -1,21 +1,22 @@
 package trellis
 
+// based on github.com/adafruit/Adafruit_Trellis_Library and
+// github.com/adafruit/Adafruit_CircuitPython_Trellis
+
 import (
-	"fmt"
 	"time"
 
 	"tinygo.org/x/drivers"
 )
 
-// HT16K33 Command Contstants
+// Default address = 0x70
+
 const (
 	HT16K33_OSCILATOR_ON    = 0x21
 	HT16K33_BLINK_CMD       = 0x80
 	HT16K33_BLINK_DISPLAYON = 0x01
 	HT16K33_CMD_BRIGHTNESS  = 0xE0
 	HT16K33_KEY_READ_CMD    = 0x40
-
-	Address = 0x70
 )
 const (
 	HT16K33_BLINK_OFF    = 0
@@ -43,96 +44,75 @@ var (
 )
 
 type Device struct {
-	bus     drivers.I2C
-	Address uint16
+	bus        drivers.I2C
+	address    uint16
+	brightness uint8
 }
 
-func New(bus drivers.I2C) Device {
-	return Device{bus, Address}
+func New(bus drivers.I2C, address uint16, brightness uint8) Device {
+	return Device{bus, address, brightness}
 }
 
 func (d Device) Configure() {
-	//d.bus.WriteRegister(uint8(d.Address), HT16K33_OSCILATOR_ON, []uint8{0})
-	println("Begin Configure")
-	// need this or no lights
-	err := d.bus.Tx(d.Address, []byte{0x21}, nil)
+	//println("Begin Configure")
+	err := d.bus.Tx(d.address, []byte{HT16K33_OSCILATOR_ON}, nil)
 	if err != nil {
 		println("Could not turn oscillator on:", err)
 		return
 	}
 	time.Sleep(1 * time.Second)
-	//d.blinkRate(HT16K33_BLINK_2HZ)
-	d.blinkRate(0)
-	/*
-		d.setBrightness(15) // max brightness
-		d.bus.Tx(d.Address, []byte{0xA1}, nil)
-	*/
+	d.blinkRate(HT16K33_BLINK_OFF)
+	d.setBrightness(d.brightness) // max brightness = 15
+	d.bus.Tx(d.address, []byte{0xA1}, nil)
 }
 
-//bool Adafruit_Trellis::isKeyPressed(uint8_t k) {
-func isKeyPressed(k uint8) bool {
+func IsKeyPressed(k uint8) bool {
 	if k > 15 {
 		return false
 	}
 	key := buttonLUT[k]
-	//#define _BV(bit) (1 << (bit))
 	if (keys[key>>4] & (1 << (key & 0x0F))) != 0 {
 		return true
-	} else {
-		return false
 	}
+	return false
 }
 
-//bool Adafruit_Trellis::wasKeyPressed(uint8_t k) {
-func wasKeyPressed(k uint8) bool {
+func WasKeyPressed(k uint8) bool {
 	if k > 15 {
 		return false
 	}
 	key := buttonLUT[k]
+	//fmt.Printf("%d: %d: %d - %x & %x\r\n", k, key, key>>4, lastkeys[key>>4], (1 << (key & 0x0F)))
 	if (lastkeys[key>>4] & (1 << (key & 0x0F))) != 0 {
 		return true
-	} else {
-		return false
 	}
+	return false
 }
 
-//boolean Adafruit_Trellis::justPressed(uint8_t k) {
-func justPressed(k uint8) bool {
-	return (isKeyPressed(k) && !wasKeyPressed(k))
+func JustPressed(k uint8) bool {
+	return (IsKeyPressed(k) && !WasKeyPressed(k))
 }
 
-//boolean Adafruit_Trellis::justReleased(uint8_t k) {
 func justReleased(k uint8) bool {
-	return (!isKeyPressed(k) && wasKeyPressed(k))
+	return (!IsKeyPressed(k) && WasKeyPressed(k))
 }
 
-//boolean Adafruit_Trellis::isLED(uint8_t x) {
 func isLED(x uint8) bool {
 	if x > 15 {
 		return false
 	}
 	led := ledLUT[x]
-	//#define _BV(bit) (1 << (bit))
 	return (displaybuffer[led>>4]&(1<<(led&0x0F)) > 0)
 }
 
-//void Adafruit_Trellis::setLED(x uint8) {
 func SetLED(x uint8) {
 	if x > 15 {
 		return
 	}
-	//led := ledLUT[x]
-	println("x =", x)
-	x = ledLUT[x]
-	//println("led =", led)
-	println("x =", x)
-	//displaybuffer[led>>4] |= (1 << (led & 0x0F))
-	//#define _BV(bit) (1 << (bit))
-	displaybuffer[x>>4] |= (1 << (x & 0x0F))
-	fmt.Printf("db = %v\r\n", displaybuffer)
+	led := ledLUT[x]
+	displaybuffer[led>>4] |= (1 << (led & 0x0F))
 }
 
-//void Adafruit_Trellis::clrLED(x uint8) {
 func clrLED(x uint8) {
 	if x > 15 {
 		return
@@ -141,37 +121,44 @@ func clrLED(x uint8) {
 	displaybuffer[led>>4] &= (1 << (led & 0x0F))
 }
 
-//boolean Adafruit_Trellis::readSwitches(void) {
-func (d Device) readSwitches() bool {
-	//memcpy(lastkeys, keys, sizeof(keys));
+func (d Device) ReadSwitches() bool {
 	lastkeys := keys
-
-	//Wire.write(0x40);
-	//Wire.requestFrom((byte)i2c_addr, (byte)6);
 	var buf [6]byte
-	d.bus.Tx(d.Address, []byte{uint8(0x40)}, buf[:])
-
-	/*
-		  Wire.requestFrom((byte)i2c_addr, (byte)6);
-			for i:=0; i<6; i++
-		    keys[i] = Wire.read();
-	*/
-
+	d.bus.Tx(d.address, []byte{0x40}, buf[:])
 	copy(keys[:], buf[:])
 
 	for i := 0; i < 6; i++ {
 		if lastkeys[i] != keys[i] {
+			//fmt.Printf("lastkeys = %v\r\n", lastkeys)
+			//fmt.Printf("keys = %v\r\n", keys)
 			return true
 		}
 	}
 	return false
 }
 
+func (d Device) ReadSingleSwitch() (bool, int) {
+	var buf [6]byte
+	d.bus.Tx(d.address, []byte{uint8(0x40)}, buf[:])
+
+	for i := 0; i < 6; i++ {
+		if buf[i] != 0 {
+			for k := 0; k < 16; k++ {
+				key := buttonLUT[k]
+				if (buf[key>>4] & (1 << (key & 0x0F))) != 0 {
+					return true, k
+				}
+			}
+		}
+	}
+	return false, 0
+}
+
 func (d Device) setBrightness(b uint8) {
 	if b > 15 {
 		b = 15
 	}
-	d.bus.Tx(d.Address, []byte{HT16K33_CMD_BRIGHTNESS | b}, nil)
+	d.bus.Tx(d.address, []byte{HT16K33_CMD_BRIGHTNESS | b}, nil)
 }
 
 func (d Device) blinkRate(b uint8) {
@@ -179,7 +166,7 @@ func (d Device) blinkRate(b uint8) {
 		b = 0
 	}
 	x := HT16K33_BLINK_CMD | HT16K33_BLINK_DISPLAYON | (b << 1)
-	err := d.bus.Tx(d.Address, []byte{x}, nil)
+	err := d.bus.Tx(d.address, []byte{x}, nil)
 	if err != nil {
 		println("Could not set blink rate")
 		return
@@ -193,7 +180,7 @@ func (d Device) WriteDisplay() {
 		buf[2*i+1] = uint8(displaybuffer[i] & 0xFF)
 		buf[2*i+2] = uint8(displaybuffer[i] >> 8)
 	}
-	err := d.bus.Tx(d.Address, buf[:], nil)
+	err := d.bus.Tx(d.address, buf[:], nil)
 	if err != nil {
 		println("Could not write display")
 		return
